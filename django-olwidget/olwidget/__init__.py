77806e0cb054dee57f58580794dcb18772d3debe
Mike C. Fletcher <mcfletch@vrplumber.com>
2015-10-05 13:59:44 -0400

    More reversals of upstream changes, we don't want that...

--- django-olwidget/olwidget/__init__.py
+++ django-olwidget/olwidget/__init__.py
@@ -1,8 +1,1 @@
-import copy
-import django.utils
-import json
-import sys
-sys.modules["django.utils"].copycompat = copy
-sys.modules["django.utils.copycompat"] = copy
-sys.modules["django.utils"].simplejson = json
-sys.modules["django.utils.simplejson"] = json
+
