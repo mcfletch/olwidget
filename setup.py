0d518871203313acea08f46e48bff177c59b46bc
Mike C. Fletcher <mcfletch@vrplumber.com>
2016-06-15 04:09:02 -0400

    Try to get a clean installation with all files

--- setup.py
+++ setup.py
@@ -24,30 +24,31 @@ setup(name='django-olwidget',
     url='http://olwidget.org',
     packages=['olwidget'],
     package_dir={'': 'django-olwidget'},
-    data_files=[
-        ('olwidget/templates/olwidget', [
-            'django-olwidget/olwidget/templates/olwidget/admin_olwidget.html',
-            'django-olwidget/olwidget/templates/olwidget/editable_layer.html',
-            'django-olwidget/olwidget/templates/olwidget/multi_layer_map.html',
-            'django-olwidget/olwidget/templates/olwidget/info_layer.html',
-            'django-olwidget/olwidget/templates/olwidget/test_map_template.html',
-        ]),
-        ('olwidget/static/olwidget/css', [
-            'css/olwidget.css'
-        ]),
-        ('olwidget/static/olwidget/js', [
-            'js/olwidget.js',
-            'js/cloudmade.js',
-        ]),
-        ('olwidget/static/olwidget/img', [
-            'img/extra_edit_icons.png',
-            'img/jquery_ui_license.txt',
-            'img/popup_icons.png',
-        ]),
-        ('olwidget/templates/admin', [
-            'django-olwidget/olwidget/templates/admin/olwidget_change_list.html'
-        ]),
-    ],
+    include_package_data = True,
+#    data_files=[
+#        ('olwidget/templates/olwidget', [
+#            'django-olwidget/olwidget/templates/olwidget/admin_olwidget.html',
+#            'django-olwidget/olwidget/templates/olwidget/editable_layer.html',
+#            'django-olwidget/olwidget/templates/olwidget/multi_layer_map.html',
+#            'django-olwidget/olwidget/templates/olwidget/info_layer.html',
+#            'django-olwidget/olwidget/templates/olwidget/test_map_template.html',
+#        ]),
+#        ('olwidget/static/olwidget/css', [
+#            'css/olwidget.css'
+#        ]),
+#        ('olwidget/static/olwidget/js', [
+#            'js/olwidget.js',
+#            'js/cloudmade.js',
+#        ]),
+#        ('olwidget/static/olwidget/img', [
+#            'img/extra_edit_icons.png',
+#            'img/jquery_ui_license.txt',
+#            'img/popup_icons.png',
+#        ]),
+#        ('olwidget/templates/admin', [
+#            'django-olwidget/olwidget/templates/admin/olwidget_change_list.html'
+#        ]),
+#    ],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
